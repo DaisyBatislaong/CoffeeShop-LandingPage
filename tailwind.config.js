82491b38f82e7e0cd1f9919aab74f8c7cbbc849d
DaisyBatislaong <daisyjanebatislaong1124@gmail.com>
2020-10-05 15:47:57 +0800

    change all to tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    future: {
+        // removeDeprecatedGapUtilities: true,
+        // purgeLayersByDefault: true,
+    },
+    purge: [],
+    theme: {
+        extend: {
+            colors: {
+                mycolor: "#090605",
+                accent1: "#E2B885",
+                accent2: "#57250F",
+                accent3: "#C89B6C",
+                accent4: "#966D49",
+                graydark: "#6A5241",
+                textcolor: "#ECE8E5",
+                mygrey: "837C83",
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+};
